243d249809f104c98ad75b551ef687ae2fc4c4f3
Edgar Tomio <edgartomio@hotmail.com>
2013-07-02 13:04:54 -0300

    Codigo php

---
+++ php_braspag.php
@@ -0,0 +1,35 @@
+<?php $parameters = new stdClass();
+$parameters->merchantId = 'C4881CB3-9953-9D5E-380A-DE1E3B7F8976'; //aqui merchantId
+$parameters->request = array(
+	 'nome = comprador',
+ 'valor = 100'
+);
+
+
+/**
+ * Aqui enviamos a requisição
+ */
+try {
+	$braspag = new SoapClient( 'https://homologacao.pagador.com.br/BraspagGeneralService/BraspagGeneralService.asmx?WSDL',
+		array(
+			'trace'			=> 1,
+			'exceptions'	=> 1,
+			'style'			=> SOAP_DOCUMENT,
+			'use'			=> SOAP_LITERAL,
+			'soap_version'	=> SOAP_1_1,
+			'encoding'		=> 'UTF-8'
+	)
+);
+
+	/**
+ 	* A variável $EncryptRequestResult abaixo conterá o conteúdo criptografado se tudo ocorrer bem
+ 	*/
+	
+$EncryptRequestResponse = $braspag->EncryptRequest( $parameters );
+
+echo $EncryptRequestResponse->EncryptRequestResult; //Exibindo o conteúdo criptografado
+} catch( SoapFault $fault ){
+		echo 'Ocorreu um erro: ' , $fault->getMessage();
+}
+
+?>
